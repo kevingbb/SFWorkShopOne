f7dc74bcd258f4e4d8a56b9a3c221ee096616fa3
Kevin Harris (CANADA) <kevinhar@microsoft.com>
2016-10-14 16:22:11 -0400

    Added doubles feature.

---
+++ FrontEndWebServer/Controllers/DoublesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Fabric;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+
+namespace FrontEndWebServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class DoublesController : Controller
+    {
+        private static readonly Uri serviceUri;
+
+        static DoublesController()
+        {
+            serviceUri = new Uri(FabricRuntime.GetActivationContext().ApplicationName + "/StatefulOne");
+        }
+
+        // GET api/doubles
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string NoNumber = "No Number Provided.";
+            System.Diagnostics.Trace.WriteLine(NoNumber);
+            return new string[] { NoNumber };
+        }
+
+        // GET api/doubles/5
+        [HttpGet("{number}")]
+        public IEnumerable<string> Get(double number)
+        {
+            double numberDoubled = 0;
+            numberDoubled = Math.Pow(number, 2);
+
+            System.Diagnostics.Trace.WriteLine(String.Format("Number Doubled: {0}", numberDoubled));
+            return new string[] { numberDoubled.ToString() };
+        }
+
+        // POST api/doubles
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/doubles/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/doubles/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
